388b56a7fb1e05fc0e9a2b19d57d83c4c1202710
swaraj-2895 <65616505+swaraj-2895@users.noreply.github.com>
2020-07-12 17:09:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def pred():
     if request.method=='POST':
         links=get_links()
         heading=headings(links)
-        job = q.enqueue(predictions, args=(links, heading), result_ttl=-1)
+        job = q.enqueue(predictions, args=(links, heading), result_ttl=-1, job_timeout=3600)
         head_sum, text_sum=job.result
  
     return render_template('text_sum.html', head_sum=head_sum, text_sum=text_sum)
